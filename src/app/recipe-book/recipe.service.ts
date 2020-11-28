355bedfb8290db279f3d7b8b800b3bdedab75394
theonly1me <pulavarthi.preetham@gmail.com>
2020-11-29 03:52:53 +0530

    added back template recipes

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -6,28 +6,28 @@ import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-  private recipes: Recipe[] = [];
-
-  // new Recipe(
-  //   `Margherita Pizza`,
-  //   `Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil.`,
-  //   `https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg`,
-  //   [
-  //     new Ingredient('Tomatoes', 2),
-  //     new Ingredient('Cheese Slices', 4),
-  //     new Ingredient('Basil Leaves', 10),
-  //   ]
-  // ),
-  // new Recipe(
-  //   `Tatertot Casserole`,
-  //   `Quick and easy casserole everyone will love!`,
-  //   `https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2012/6/15/0/CC_Hot-Dish-Tater-Tot-Casserole-Recipe_s4x3.jpg.rend.hgtvcom.826.620.suffix/1358449564114.jpeg`,
-  //   [
-  //     new Ingredient('Tater Tots', 10),
-  //     new Ingredient('Carrots', 3),
-  //     new Ingredient('Beans', 10),
-  //   ]
-  // ),
+  private recipes: Recipe[] = [
+    new Recipe(
+      `Margherita Pizza`,
+      `Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil.`,
+      `https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg`,
+      [
+        new Ingredient('Tomatoes', 2),
+        new Ingredient('Cheese Slices', 4),
+        new Ingredient('Basil Leaves', 10),
+      ]
+    ),
+    new Recipe(
+      `Tatertot Casserole`,
+      `Quick and easy casserole everyone will love!`,
+      `https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2012/6/15/0/CC_Hot-Dish-Tater-Tot-Casserole-Recipe_s4x3.jpg.rend.hgtvcom.826.620.suffix/1358449564114.jpeg`,
+      [
+        new Ingredient('Tater Tots', 10),
+        new Ingredient('Carrots', 3),
+        new Ingredient('Beans', 10),
+      ]
+    ),
+  ];
 
   updatedRecipe = new Subject<Recipe[]>();
 
